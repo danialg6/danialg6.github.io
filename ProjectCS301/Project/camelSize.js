f64ecf1b08d5100ed0e0d9e8447445eb446ba37c
Danial5621 <78656217+Danial5621@users.noreply.github.com>
2021-03-26 02:32:15 -0500

    update

---
+++ ProjectCS301/Project/camelSize.js
@@ -0,0 +1,11 @@
+function camelize(str) {
+    let result=[];
+
+   str=str.split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
+   result.push(str[0])
+  for(let i=1;i<str.length;i++){
+   result.push(str[i][0].toUpperCase()+str[i].slice(1))
+  }
+  return   result.join(""); // joins ['my', 'Long', 'Word'] into 'myLongWord'
+}
+console.log(camelize("hello-world-works-runs"))
