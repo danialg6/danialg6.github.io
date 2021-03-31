42fbb04b9d557baf2eae1fa6c623234d5ebbb2d1
Danial5621 <78656217+Danial5621@users.noreply.github.com>
2021-03-31 00:17:18 -0500

    upgraded

--- CS303ObjectOrientedToJavaScript/W2D2/mayMap.js
+++ CS303ObjectOrientedToJavaScript/W2D2/mayMap.js
@@ -1,44 +1,40 @@
 "use strict";
 // //map
-// /**
-//  * 
-//  * @param {object} arr array 
-//  * @param {function} fn function
-//  * @returns {object} object
-//  */
-// function myMap(arr, fn){
-//     let result = [];
-//     for(let i =0;i<arr.length;i++){
-//     let callbackResult = fn(arr[i],i);
-//     if(callbackResult){
-//     result.push(arr[i]);
-//     }
-//     return result;
-//     }
-//     return result;
-//     }
-//     console.log(myMap([1,2,3,4,5]));
-//      myMap(arr,function(value,index){
-//      return value*2;
-//      });
+/**
+ * 
+ * @param {object} arr array 
+ * @param {function} fn function
+ * @returns {object} object
+ */
+function myMap(arr, fn){
+    let result = fn(arr);
+    return result;
+    }
+    // function arrTimesthree(arr) {
+    //     let result=[];
+    //     for(let values of arr){
+    //         result.push(values*3);
+    //     }
+    //     return result;
+    // }
 
     //filter
     
-    function myFilter(arr,fn){
-        let result = [];
-        for(let i =0;i<arr.length;i++){
-        let callbackResult = fn(arr[i],i);
-        if(callbackResult){
-        result.push(arr[i]);
-        }
-        }
-        return result;
-        }
+    // function myFilter(arr,fn){
+    //     let result = [];
+    //     for(let i =0;i<arr.length;i++){
+    //     let callbackResult = fn(arr[i],i);
+    //     if(callbackResult){
+    //     result.push(arr[i]);
+    //     }
+    //     }
+    //     return result;
+    //     }
         
-        myFilter(arr,function(value,index){
-        return value%2===0;
-        });
-        let arr=[1,2,3,4];
-        console.log(myFilter(arr,function() {
+    //     myFilter(arr,function(value,index){
+    //     return value%2===0;
+    //     });
+    //     let arr=[1,2,3,4];
+    //     console.log(myFilter(arr,function() {
             
-        }));
+    //     }));
