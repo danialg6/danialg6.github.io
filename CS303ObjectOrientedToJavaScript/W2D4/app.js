947b57c29bef8f63bfb433e8445b681036ca0b00
Danial5621 <78656217+Danial5621@users.noreply.github.com>
2021-04-01 23:18:37 -0500

    W2D2final

--- CS303ObjectOrientedToJavaScript/W2D4/app.js
+++ CS303ObjectOrientedToJavaScript/W2D4/app.js
@@ -28,3 +28,70 @@ let search2 = "crush";
  
 console.log(lisa.value === search1);
 console.log(bart.value === search1);
+
+///Q
+"use strict";
+/*eslint-disable*/
+const bank = {
+    transactionDB: [],
+};
+bank.transactionDB = [
+    { custID: 1, custTrans: [10, 50, -40] }, // balance = 20
+    { custID: 2, custTrans: [10, 10, -10] }, // balance = 10
+    { custID: 3, custTrans: [5, -5, 55] }, // balance = 55
+];
+bank.checkId = function (id) {
+    const foundCustmr = bank.transactionDB.find(customer => customer.custID === id);
+    return foundCustmr;
+};
+ 
+bank.getBalance = function (id) {
+    const customer = this.checkId(id);
+    const balance = customer.custTrans.reduce((sum, item) => sum + item, 0);
+    return balance
+}
+ 
+bank.saveTransaction = function (id, amount) {
+    const customer = this.checkId(id);
+    customer.custTrans.push(amount);
+}
+ 
+bank.debit = function (id, amount) {
+    let balance = this.getBalance(id);
+    if (amount < 0) {
+        console.log("Invalid amount. Please enter positive amount !");
+    } else if (amount > balance) {
+        console.log("Insufficient balance. Please try again !");
+    } else {
+        amount = amount * -1;
+        balance += amount;
+        this.saveTransaction(id, amount);
+    }
+    return balance;
+}
+ 
+bank.credit = function (id, amount) {
+    let balance = this.getBalance(id);
+    if (amount < 0) {
+        console.log("Invalid amount. Please enter positive amount !");
+    } else {
+        balance += amount;
+        this.saveTransaction(id, amount);
+    }
+    return balance;
+}
+ 
+bank.totalBalance = function () {
+    const custArr = this.transactionDB;
+    let sum = 0;
+    for (const customer of custArr) {
+        const id = customer.custID;
+        const custBalance = this.getBalance(id);
+        sum += custBalance;
+    }
+    return sum;
+}
+console.log("total balance should be 85: ", bank.bankBalance());
+bank.credit(1, 20);
+bank.debit(1, 1000);
+console.log("total should now be 105: ", bank.bankBalance());
