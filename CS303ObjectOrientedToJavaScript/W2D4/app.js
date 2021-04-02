a0eb2949cda7c075767f1c022c7c15ff3d70a5ea
Danial5621 <78656217+Danial5621@users.noreply.github.com>
2021-04-01 23:12:36 -0500

    final

--- CS303ObjectOrientedToJavaScript/W2D4/app.js
+++ CS303ObjectOrientedToJavaScript/W2D4/app.js
@@ -1,13 +1,30 @@
 "use strict";
-/*eslient-disable*/
-function myMap(arr,fun){
-    let newArray=[];
-    for(let elements of arr){
-    newArray.push(fun(elements))
-    }
-    return newArray;
-    }
-    let multByThree=value=>value*3;
-    
-    let num=[1,2,3,4,5];
-    console.log(myMap(arr,multByThree));
+/**
+ * @param {String} value to
+ * @return{String} value
+ */
+function TreeNode(value) {
+    this.value = value;
+    this.descendant = [];
+    return this.value;
+}
+// create nodes with values
+const abe = new TreeNode("Abe");
+const homer = new TreeNode("Homer");
+const bart = new TreeNode("Bart");
+const lisa = new TreeNode("Lisa");
+const maggie = new TreeNode("Maggie");
+abe.descendant.push(homer);
+homer.descendant.push(bart, lisa, maggie);
+//Q1.
+console.log(abe.value);
+console.log(homer.value);
+console.log(bart.value);
+console.log(lisa.value);
+console.log(maggie.value);
+//Q2.
+let search1 = "Lisa";
+let search2 = "crush";
+ 
+console.log(lisa.value === search1);
+console.log(bart.value === search1);
