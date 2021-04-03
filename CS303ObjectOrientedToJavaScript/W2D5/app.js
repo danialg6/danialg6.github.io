3d32b5b8368bf66dac19765e69927e020c9a0442
Danial5621 <78656217+Danial5621@users.noreply.github.com>
2021-04-03 00:18:43 -0500

    updatedFinal

---
+++ CS303ObjectOrientedToJavaScript/W2D5/app.js
@@ -0,0 +1,65 @@
+"use strict";
+/*eslient-disable*/
+//1. Filter through function
+
+//Filter inBetween
+
+function inBetween(a, b) {
+  return function(x) {
+    return x >= a && x <= b;
+  };
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+console.log( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+
+//Filter inArray
+function inArray(arr) {
+    return x => arr.includes(x);
+  }
+  let arrr = [1, 2, 3, 4, 5, 6, 7];
+  console.log( arrr.filter(inArray([1, 2, 10])) ); // 1,2
+
+  //Sort by field
+  function byField(fieldName){
+    return (a, b) => a[fieldName] > b[fieldName] ? 1 : -1;
+  }
+  let users = [
+    { name: "John", age: 20, surname: "Johnson" },
+    { name: "Pete", age: 18, surname: "Peterson" },
+    { name: "Ann", age: 19, surname: "Hathaway" }
+  ];
+  // by name (Ann, John, Pete)
+users.sort((a, b) => a.name > b.name ? 1 : -1);
+
+// by age (Pete, Ann, John)
+users.sort((a, b) => a.age > b.age ? 1 : -1);
+//
+console.log(users.sort(byField('name')));
+console.log(users.sort(byField('age')));
+
+//
+//Army of functions
+function makeArmy() {
+    let shooters = [];
+  
+    let i = 0;
+    while (i < 10) {
+        let shooter = function() { 
+            console.log("I am shooter ", i); 
+            return i;
+            };
+      shooters.push(shooter); // and add it to the array
+      i++;
+    }
+  
+    // ...and return the array of shooters
+    return shooters;
+  }
+  
+  let army = makeArmy();
+  
+  // all shooters show 10 instead of their numbers 0, 1, 2, 3...
+  army[0](); // 10 from the shooter number 0
+  army[1](); // 10 from the shooter number 1
+  army[2](); // 10 ...and so on.
--- CS303ObjectOrientedToJavaScript/W2D5/latestValue1.js
+++ CS303ObjectOrientedToJavaScript/W2D5/latestValue1.js
@@ -1,5 +1,8 @@
 "use strict";
 /*eslient-disable*/
+
+//Q1
+
 /*Does a function pickup latest changes?
 
 let name = "John";
@@ -17,3 +20,145 @@ Old variable values are not saved anywhere. When a function wants a
 its own Lexical Environment or the outer one.
 */
 
+//Q2
+
+//Which variables are available?
+/*
+function makeWorker() {
+  let name = "Pete";
+  return function() {
+    alert(name);
+  };
+}
+let name = "John";
+// create a function
+let work = makeWorker();
+// call it
+work(); // what will it show?
+
+///Answer
+
+The answer is: Pete.
+
+The work() function in the code below gets name from the 
+place of its origin through the outer lexical environment reference:
+So, the result is "Pete" here.
+But if there were no let name in makeWorker(),
+ then the search would go outside and take the global 
+ variable as we can see 
+from the chain above. In that case the result
+ would be "John".
+*/
+
+//Q3
+//Are counters independent?
+/* 
+function makeCounter() {
+  let count = 0;
+
+  return function() {
+    return count++;
+  };
+}
+
+let counter = makeCounter();
+let counter2 = makeCounter();
+
+alert( counter() ); // 0
+alert( counter() ); // 1
+
+alert( counter2() ); // ?
+alert( counter2() ); // ?
+
+//Answer
+
+The answer:yes counters are independent, the values are  0,1.
+
+Functions counter and counter2 are created by different 
+invocations of makeCounter.
+
+So they have independent outer Lexical Environments,
+ each one has its own count.
+*/
+
+//Q4
+/*
+//Counter object
+//Answer: the ut put is 1,2,1;
+Surely it will work just fine.
+Both nested functions are created within the same outer Lexical Environment, 
+so they share access to the same count variable: 
+*/
+function Counter() {
+  let count = 0;
+
+  this.up = function() {
+    return ++count;
+  };
+
+  this.down = function() {
+    return --count;
+  };
+}
+
+let counter = new Counter();
+
+alert( counter.up() ); // 1
+alert( counter.up() ); // 2
+alert( counter.down() ); // 1
+
+///Q5
+
+/*
+//Function in if
+Look at the code. What will be the result of the call at the last ?
+
+Answer: The result is error
+The function sayHi is declared inside the if, so it only lives inside it. 
+There is no sayHi outside.
+
+let phrase = "Hello";
+
+if (true) {
+  let user = "John";
+
+  function sayHi() {
+    alert(`${phrase}, ${user}`);
+  }
+}
+
+sayHi();
+*/
+
+//Q6
+//Sum with closures
+/*
+Write function sum that works like this: sum(a)(b) = a+b.
+
+Yes, exactly this way, using double parentheses (not a mistype).
+For instance:
+sum(1)(2) = 3
+sum(5)(-1) = 4
+*/
+
+function sum(a) {
+
+  return function(b) {
+    return a + b; // takes "a" from the outer lexical environment
+  };
+
+}
+
+alert( sum(1)(2) ); // 3
+alert( sum(5)(-1) ); // 4
+
+//Q7
+//Is variable visible?
+
+//Answer:The result is: error, because we can't access "x" before initialization
+function func() {
+  console.log(x); // ReferenceError: Cannot access 'x' before initialization
+  let x = 2;
+}
+
+func();
