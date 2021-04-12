702fc6cb376fe90bbeaeed1eba68f4850e6c5067
Danial5621 <78656217+Danial5621@users.noreply.github.com>
2021-04-11 20:39:31 -0500

    updateeed

--- CS303ObjectOrientedToJavaScript/W3D6/addMethodeDefer11.js
+++ CS303ObjectOrientedToJavaScript/W3D6/addMethodeDefer11.js
@@ -1,20 +1,20 @@
-"use strict";
-/*eslient-disable*/
-//Add method "f.defer(ms)" to functions
-// function Dispaly(name){
-//     this.name=name;
-//     this.age=25;
-// }
+// "use strict";
+// /*eslient-disable*/
+// //Add method "f.defer(ms)" to functions
+// // function Dispaly(name){
+// //     this.name=name;
+// //     this.age=25;
+// // }
 
-// let display=new Dispaly();
-// display.defer="ms";
-// console.log(display.defer);
-Function.prototype.defer = function(ms) {
-    setTimeout(this, ms);
-  };
+// // let display=new Dispaly();
+// // display.defer="ms";
+// // console.log(display.defer);
+// Function.prototype.defer = function(ms) {
+//     setTimeout(this, ms);
+//   };
   
-  function f() {
-    console.log("Hello!");
-  }
+//   function f() {
+//     console.log("Hello!");
+//   }
   
-  f.defer(1000); // shows "Hello!" after 1 sec
+//   f.defer(1000); // shows "Hello!" after 1 sec
--- CS303ObjectOrientedToJavaScript/W3D6/dufferencecalls.js
+++ CS303ObjectOrientedToJavaScript/W3D6/dufferencecalls.js
@@ -1,25 +1,25 @@
-"use strict";
+// "use strict";
 
-const { Console } = require("node:console");
+// const { Console } = require("node:console");
 
-/*eslient-disable*/
-function Rabbit(name) {
-    this.name = name;
-  }
-  Rabbit.prototype.sayHi = function() {
-    Console.console.log();( this.name );
-  }
+// /*eslient-disable*/
+// function Rabbit(name) {
+//     this.name = name;
+//   }
+//   Rabbit.prototype.sayHi = function() {
+//     console.log();( this.name );
+//   }
   
-  let rabbit = new Rabbit("Rabbit");
+//   let rabbit = new Rabbit("Rabbit");
 
-  //Question
-  /*Answer:
-The first call has this == rabbit, the other ones have this equal to Rabbit.prototype, because it’s actually the object before the dot.
+//   //Question
+//   /*Answer:
+// The first call has this == rabbit, the other ones have this equal to Rabbit.prototype, because it’s actually the object before the dot.
 
-So only the first call shows Rabbit, other ones show undefined:
-  */
+// So only the first call shows Rabbit, other ones show undefined:
+//   */
   
-  rabbit.sayHi();                        // Rabbit
-  Rabbit.prototype.sayHi();              // undefined
-  Object.getPrototypeOf(rabbit).sayHi(); // undefined
-  rabbit.__proto__.sayHi();  
+//   // rabbit.sayHi();                        // Rabbit
+//   // Rabbit.prototype.sayHi();              // undefined
+//   // Object.getPrototypeOf(rabbit).sayHi(); // undefined
+//   // rabbit.__proto__.sayHi();  
